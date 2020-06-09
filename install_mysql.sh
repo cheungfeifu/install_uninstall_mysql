6791b851141303103ed385e8bab5a8f76a20b29d
zhangfeihu <915401550@qq.com>
2020-06-09 16:02:08 +0800

    安装新增修改mysql数据储存目录

--- install_mysql.sh
+++ install_mysql.sh
@@ -12,4 +12,22 @@ yum install mysql-community-server -y
 
 systemctl start mysqld
 
+#修改mysql的数据目录，因为mysql默认安装在/var/lib/mysql下，该目录下文件存储太少，所以我们需要把
+#mysql的数据储存位置修改一下
+
+systemctl stop mysqld
+
+#注意！！！一下目录要换成你的目录
+
+mkdir –vp /data/mysql_data
+
+mv /var/lib/mysql /data/mysql_data/
+
+cd /var/lib
+
+ln -s /data/mysql_data/mysql /var/lib/mysql
+
+
+
+
 
