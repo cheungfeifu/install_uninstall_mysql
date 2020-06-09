65c7e9d98e526ab2af855bebaa6a2be58baa3252
zhangfeihu <915401550@qq.com>
2020-06-09 16:12:54 +0800

    安装完成会提示install mysql success

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#确保你有/dat目录，因为rpm文件会放到该目录下
+#确保你有/data目录，因为rpm文件会放到该目录下
 
 cd /data
 
 wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
-
+echo 'rpm文件下载完成'
 yum localinstall mysql57-community-release-el7-8.noarch.rpm -y
 
 yum install mysql-community-server -y
-
+echo 'mysql yun安装完成'
 systemctl start mysqld
 
 #修改mysql的数据目录，因为mysql默认安装在/var/lib/mysql下，该目录下文件存储太少，所以我们需要把
@@ -17,7 +17,7 @@ systemctl start mysqld
 
 systemctl stop mysqld
 
-#注意！！！一下目录要换成你的目录
+#注意！！！以下目录要换成你的目录
 
 mkdir –vp /data/mysql_data
 
@@ -27,7 +27,7 @@ cd /var/lib
 
 ln -s /data/mysql_data/mysql /var/lib/mysql
 
-
+echo 'install mysql success!'
 
 
 
