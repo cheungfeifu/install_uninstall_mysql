c9ae2e9f51d3097cb10c7530446cf0dedc1cb3fc
zhangfeihu <915401550@qq.com>
2020-06-15 17:45:44 +0800

    安装完会提示

--- install_mysql_for_centos7.sh
+++ install_mysql_for_centos7.sh
@@ -9,9 +9,9 @@ echo 'rpm文件下载完成'
 yum localinstall mysql57-community-release-el7-8.noarch.rpm -y
 
 yum install mysql-community-server -y
-echo 'mysql yun安装完成'
+echo 'mysql yum安装完成'
 systemctl start mysqld
-
+echo 'mysql服务测试可以正常使用'
 #修改mysql的数据目录，因为mysql默认安装在/var/lib/mysql下，该目录下文件存储太少，所以我们需要把
 #mysql的数据储存位置修改一下
 
@@ -27,7 +27,7 @@ cd /var/lib
 
 ln -s /data/mysql_data/mysql /var/lib/mysql
 
-echo 'install mysql success!'
+echo 'mysql 安装完成！请执行： grep 'temporary password' /var/log/mysqld.log 获取你的root密码！'
 
 
 
