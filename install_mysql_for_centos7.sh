921db1451e5a320420aea5d89ac9717bba3831a7
zhangfeihu <915401550@qq.com>
2020-09-03 17:48:10 +0800

    在centos7安装会修改为清华镜像源安装

--- install_mysql_for_centos7.sh
+++ install_mysql_for_centos7.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-#确保你有/data目录，因为rpm文件会放到该目录下
+#确保在你的根目录下创建一个data目录1.rpm文件会放到该目录下;2.mysql的数据目录也会存放在该目录下
 
 cd /data
 
 wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
 echo 'rpm文件下载完成'
 yum localinstall mysql57-community-release-el7-8.noarch.rpm -y
+
+#修改yum源的url为清华镜像源，体现飞一般的速度,注意在下面的sed命令需要转义，因为替换的文本中存在//
+
+sed -i 's#http://repo.mysql.com#https://mirrors.tuna.tsinghua.edu.cn#g' /etc/yum.repos.d/mysql-community.repo
 
 yum install mysql-community-server -y
 echo 'mysql yum安装完成'
@@ -18,8 +22,11 @@ echo 'mysql服务测试可以正常使用'
 systemctl stop mysqld
 
 #注意！！！以下目录要换成你的目录
-
 mkdir -p /data/mysql_data
+
+cd /data
+
+chown mysql:mysql mysql_data
 
 mv /var/lib/mysql /data/mysql_data/
 
